50290b8ef04d2be973649797e73a1178a35c9218
Faziz <farisaziz22@gmail.com>
2022-02-28 07:42:46 +0700

    edit testing file

--- file.js
+++ file.js
@@ -10,10 +10,28 @@ function exportFile (obj) {
 
 function importFile(file) {
     var reader = new FileReader();
+    idxPP = 0
+    idxPe = 0
+    idxG = 0
+    idxPo= 0
 
     reader.onload = function(e){
         arrObjects = JSON.parse(e.target.result);
         render(arrObjects)
+        arrObjects.forEach(item => {
+            if (item.type==persegi) {
+                idxPe+=1
+            }
+            else if (item.type==persegipanjang) {
+                idxPP +=1
+            }
+            else if (item.type==garis) {
+                idxG +=1
+            }
+            else if (item.type==poligon) {
+                idxPo +=1
+            }
+        });
         alert('File berhasil dilanjutkan')
     }
     
